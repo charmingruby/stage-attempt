6f64dfe88b51359e25c077af56f8d3c84c40d69c
charmingruby <mrrubycanal3105@gmail.com>
2024-01-09 16:18:34 -0300

    feat: evaluation unit test

--- internal/domain/evaluation_test.go
+++ internal/domain/evaluation_test.go
@@ -1,7 +1,34 @@
 package domain
 
-import "testing"
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestEvaluationJSONSerialization(t *testing.T) {
+	evaluation := NewEvaluation(
+		1,
+		1,
+		1,
+		5,
+		"quick delivery, i loved!",
+	)
+
+	// Serialization
+	data, err := json.Marshal(evaluation)
+	assert.NoError(t, err)
+
+	// Deserialization
+	var newEvaluation Evaluation
+	err = json.Unmarshal(data, &newEvaluation)
+	assert.NoError(t, err)
+
+	assert.Equal(t, evaluation.ID, newEvaluation.ID)
+	assert.Equal(t, evaluation.Comment, newEvaluation.Comment)
+	assert.Equal(t, evaluation.CustomerID, newEvaluation.CustomerID)
+	assert.Equal(t, evaluation.OrderID, newEvaluation.OrderID)
+	assert.Equal(t, evaluation.Rate, newEvaluation.Rate)
 
 }
